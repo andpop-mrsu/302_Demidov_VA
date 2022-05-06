f4f90c5a55e8a961c54674eb33abd5abcc508f0f
FunGvey <fungvey@gmail.com>
2022-05-06 23:30:20 +0300

    Task04

---
+++ Task04/src/StackInterface.php
@@ -0,0 +1,17 @@
+<?php
+
+interface StackInterface
+{
+    public function push(...$elements): void;
+
+    public function pop();
+
+    public function top();
+
+    public function isEmpty(): bool;
+
+    public function copy(): Stack;
+
+    public function __toString(): string;
+}
+
---
+++ Task04/src/run_test.php
@@ -0,0 +1,9 @@
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+require 'StackInterface.php';
+require 'Stack.php';
+require 'compareStrings.php';
+require 'Test.php';
+
+run_test();
